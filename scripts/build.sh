ebd56eb9c6feae150f308ecca48d05ab1898f56d
liuqi <liuqi@epaynfc.com>
2022-05-04 21:53:17 +0800

    build

---
+++ scripts/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if ! hash electron-packager 2>/dev/null; then
+  RED='\033[0;31m'
+  NC='\033[0m'
+  echo "${RED}Error${NC}: you need to npm install electron-packager. Aborting."
+  exit 1
+fi
+
+if [ "$#" -ne 2 ]; then
+  echo -e "Usage: ./script/build.sh <platform> <arch>"
+  echo -e "	platform:	darwin, linux, win32"
+  echo -e "	arch:		ia32, x64"
+  # exit 1
+fi
+
+PLATFORM=darwin
+ARCH=x64
+
+echo "Start packaging for $PLATFORM $ARCH."
+
+
+ APP_NAME="Net-music-cloud-uploader"
+# if [ $PLATFORM = "linux" ]; then
+   
+# else
+#     APP_NAME="net-music-cloud-uploader"
+# fi
+
+ignore_list="dist|scripts|\.idea|.*\.md|.*\.yml|node_modules/nodejieba"
+
+electron-packager . "${APP_NAME}" --platform=$PLATFORM --arch=$ARCH --electronVersion=1.4.15 --app-version=1.4.0 --asar --icon=assets/icon.icns --overwrite --out=./dist --ignore=${ignore_list}
+
+if [ $? -eq 0 ]; then
+  echo -e "$(tput setaf 2)Packaging for $PLATFORM $ARCH succeeded.$(tput sgr0)\n"
+fi
+
+if [ $PLATFORM = "darwin" ]; then
+    ditto -rsrcFork ./dist/Electronic\ CloudUploader-darwin-x64/Electronic\ WeChat.app /Applications/Electronic\ WeChat.app
+    echo "$(tput setaf 3)App copied to /Applications. You can open Net-music-cloud-uploader there or from Spotlight.$(tput sgr0)"
+fi
